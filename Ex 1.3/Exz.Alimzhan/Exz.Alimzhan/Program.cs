dcc149a978c6817f672d6e943041119736da9eca
Kirill <109529075+RiDdLler@users.noreply.github.com>
2022-11-29 23:34:45 +0600

    Add files via upload
    This is one version of the C# exam

---
+++ Ex 1.3/Exz.Alimzhan/Exz.Alimzhan/Program.cs
@@ -0,0 +1,28 @@
+﻿using Exz.Alimzhan.Data;
+using Exz.Alimzhan.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Timers;
+using Timer = System.Timers.Timer;
+using Exz.Alimzhan.Service;
+
+namespace Exz.Alimzhan
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.StartMenu();
+        }
+    }
+}
+       
+
+       
